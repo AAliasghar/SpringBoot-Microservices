b1e587f28c0588b0495cda56fa06cbc7f08a6793
AAliasghar <a.asgharinia@yahoo.com>
2023-05-13 18:22:32 +0200

    Employee JPA Entity

---
+++ employee-service/employee-service/src/main/java/com/ali/employeeservice/entity/Employee.java
@@ -0,0 +1,23 @@
+package com.ali.employeeservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String email;
+}
