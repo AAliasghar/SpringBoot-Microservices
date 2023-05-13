983dcee59b6229451641ff3c23f357d48a204105
AAliasghar <a.asgharinia@yahoo.com>
2023-05-13 08:19:54 +0200

    Department Dto

---
+++ department-service/department-service/src/main/java/com/ali/departmentservice/service/DepartmentService.java
@@ -0,0 +1,7 @@
+package com.ali.departmentservice.service;
+
+import com.ali.departmentservice.dto.DepartmentDto;
+
+public interface DepartmentService {
+    DepartmentDto saveDepartment( DepartmentDto departmentDto);
+}
---
+++ department-service/department-service/src/main/java/com/ali/departmentservice/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,11 @@
+package com.ali.departmentservice.service.impl;
+
+import com.ali.departmentservice.dto.DepartmentDto;
+import com.ali.departmentservice.service.DepartmentService;
+
+public class DepartmentServiceImpl implements DepartmentService {
+    @Override
+    public DepartmentDto saveDepartment(DepartmentDto departmentDto) {
+        return null;
+    }
+}
