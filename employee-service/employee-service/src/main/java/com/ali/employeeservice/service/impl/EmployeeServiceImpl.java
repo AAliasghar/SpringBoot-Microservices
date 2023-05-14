ebdf815ab564723bb915f05665930e50f3bdeedb
AAliasghar <a.asgharinia@yahoo.com>
2023-05-14 17:45:39 +0200

    WebClient

--- employee-service/employee-service/src/main/java/com/ali/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/employee-service/src/main/java/com/ali/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -8,16 +8,16 @@ import com.ali.employeeservice.mapperDto.MappingDto;
 import com.ali.employeeservice.repository.EmployeeRepository;
 import com.ali.employeeservice.service.EmployeeService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
-    private RestTemplate restTemplate;
+//    private RestTemplate restTemplate;
+    private WebClient webClient;
 
     // Save Employee
     @Override
@@ -38,10 +38,12 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         Employee employee = employeeRepository.findById(employeeId).get();
 
-        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http;//localhost:8080/api/departments" + employee.getDepartmentCode(),
-                DepartmentDto.class);
+//        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/" + employee.getDepartmentCode(),
+//                DepartmentDto.class);
+//
+//        DepartmentDto departmentDto = responseEntity.getBody();
 
-        DepartmentDto departmentDto = responseEntity.getBody();
+        DepartmentDto departmentDto  = webClient.get().uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode()).retrieve().bodyToMono(DepartmentDto.class).block();
 
         EmployeeDto employeeDto = MappingDto.employeeMappingDto(employee);
 
