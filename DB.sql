9555439db44f1ccbd369ab1ec340bde21365706a
AAliasghar <a.asgharinia@yahoo.com>
2023-05-17 23:16:33 +0200

    Load Balancing

--- DB.sql
+++ DB.sql
@@ -1,6 +1,6 @@
 -- -----------------------------------------------------
 -- Schema Department ------
------------------------------------------------------
+-- -----------------------------------------------------
 -- DROP SCHEMA IF EXISTS `department_db`;
 
 -- CREATE DATABASE `department_db`;
@@ -9,6 +9,20 @@
 -- -----------------------------------------------------
 -- TABLE DEPARTMENTS
 -- -----------------------------------------------------
-
 SELECT * FROM departments ;
+-- DELETE FROM department_db.departments where id = 2 ; 
+
+-- --------------------------------------------------------
+-- --------------------------------------------------------
+-- Schema Employee -------------------------
+-- ------------------------------------------------------
+-- DROP SCHEMA IF EXISTS `employee_db`;
+
+-- CREATE DATABASE `employee_db`;
+ USE `employee_db` ;
+
+-- -----------------------------------------------------
+-- TABLE Employees
+-- -----------------------------------------------------
+SELECT * FROM employee_db.employees ;
 -- DELETE FROM department_db.departments where id = 2 ; 
