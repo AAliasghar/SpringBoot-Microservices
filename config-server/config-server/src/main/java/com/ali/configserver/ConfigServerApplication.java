cdb17532a579e5f51c5c480fb2fc32619b22d100
AAliasghar <a.asgharinia@yahoo.com>
2023-05-20 23:27:34 +0200

    Register Config Server as Eureka Client

--- config-server/config-server/src/main/java/com/ali/configserver/ConfigServerApplication.java
+++ config-server/config-server/src/main/java/com/ali/configserver/ConfigServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableConfigServer provided auto-configuration in spring cloud 2022.0.0
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
