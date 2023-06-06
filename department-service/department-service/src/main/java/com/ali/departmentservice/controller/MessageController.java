b471ee23984263d72ed8cfe010cc761573fe2c84
AAliasghar <a.asgharinia@yahoo.com>
2023-06-06 23:19:51 +0200

    Creating Component

--- department-service/department-service/src/main/java/com/ali/departmentservice/controller/MessageController.java
+++ department-service/department-service/src/main/java/com/ali/departmentservice/controller/MessageController.java
@@ -1,6 +1,8 @@
 package com.ali.departmentservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RefreshScope
@@ -8,12 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageController {
 
     // Create the property  in department-service.properties
-//    @Value("${spring.boot.message}")
-//    private String message;
-//
-//    @GetMapping("message")
-//    public String message (){
-//        return message;
-//    }
+    @Value("${spring.boot.message}")
+    private String message;
+
+    @GetMapping("message")
+    public String message (){
+        return message;
+    }
 
 }
--- employee-service/employee-service/src/main/java/com/ali/employeeservice/controller/MessageController.java
+++ employee-service/employee-service/src/main/java/com/ali/employeeservice/controller/MessageController.java
@@ -1,21 +1,19 @@
 package com.ali.employeeservice.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RefreshScope
 @RestController
 public class MessageController {
 
-    // Create the property  in department-service.properties
-    @Value("${spring.boot.message}")
-    private String message;
-
-    @GetMapping("/users/message")
-    public String message (){
-        return message;
-    }
+    // Create the property  in employee-service.properties
+//    @Value("${spring.boot.message}")
+//    private String message;
+//
+//    @GetMapping("/users/message")
+//    public String message (){
+//        return message;
+//    }
 
 }
